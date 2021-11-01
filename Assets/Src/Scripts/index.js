fac9b5913a4c52d5f290b6c56ebf7a595411c5ab
leandro.alves <leandro.alves@jbq.global>
2021-10-31 23:49:54 -0300

    feature: component with carrousel descktop

--- Assets/Src/Scripts/index.js
+++ Assets/Src/Scripts/index.js
@@ -1,6 +1,12 @@
-function next(id, width){
+let width = window.innerWidth;
+if(width > 1023){
+    width = 312;
+} else {
+    width = 397;
+}
+function next(id){
     document.getElementById(id).scrollLeft += width;
 }
-function back(id, width){
+function back(id){
     document.getElementById(id).scrollLeft -= width;
 }
